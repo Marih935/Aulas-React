c672e8c4c8c41378b52df68b500cbb3d11ff9df1
Mariana Cavalcanti Machado Anselmo <cmariana935@gmail.com>
2023-03-10 20:22:55 -0300

    Rect router, Eslint (tentativa)

--- src/components/Exemplo01.jsx
+++ src/components/Exemplo01.jsx
@@ -1,60 +1,60 @@
-import React from "react";
-import {ReactDOM} from "react-dom/client";
-//react faz isso automaticamente
+import React from 'react';
+// react faz isso automaticamente
 
 class Timer extends React.Component {
-    
-    constructor(props){
-        super(props); //obrigatório ter super no constructor (herda)
-        this.state = { seconds: this.props.start };
+  constructor(props) {
+    super(props); // obrigatório ter super no constructor (herda)
+    cont { props };
+    this.state = { seconds: this.props.start };
+  }
+
+  contar() { // método do Timer
+    this.setState((state) => ({ seconds: state.seconds + 1 }));
+  }
+
+  startStop() {
+    if (this.interval) {
+      clearInterval(this.interval);
+      this.interval = null;
+    } else {
+      this.interval = setInterval(() => this.contar(), 1000);
     }
+  }
 
-    contar(){ //método do Timer
-        this.setState(state => ({ seconds: state.seconds + 1 })); //atributo => altera estado do componente adicionando novo estado
-    }
+  zero() {
+    this.setState(() => ({ seconds: 0 }));
+  }
 
-    startStop(){
-        if(this.interval){
-            clearInterval(this.interval);
-            this.interval = null;
-        } else{
-            this.interval = setInterval(() => this.contar(), 1000);
-        }
-    }
-    
-zero(){
-    this.setState(state => ({ seconds: 0 }));
-}
+  // componentDidMount(){ //método para "montar"
+  //     this.interval = setInterval(() => this.contar(), this.props.ms); //interval => função pra contar tempo, a cada "ms" vai mudar o estado
+  // }
 
-    // componentDidMount(){ //método para "montar"
-    //     this.interval = setInterval(() => this.contar(), this.props.ms); //interval => função pra contar tempo, a cada "ms" vai mudar o estado
-    // }
+  // componentWillUnmount(){ //método para "desmontar"
+  //     clearInterval(this.interval); //finalizar para n dar erro (só pararia de contar quando fechar o navegador)
+  // }
 
-    // componentWillUnmount(){ //método para "desmontar"
-    //     clearInterval(this.interval); //finalizar para n dar erro (só pararia de contar quando fechar o navegador)
-    // }
+  render() {
+    const { seconds } = this.state;
+    return (
+      <>
+        <div className="bg-black w-48 md:w-96 h-16 m-4 rounded-xl flex flex-col justify-center items-center">
+          <h1 className="text-3xl text-azull01 cronometro">
+            {seconds}
+          </h1>
+        </div>
 
-    render(){
-        return(
-            <>
-                <div className="bg-black w-48 md:w-96 h-16 m-4 rounded-xl flex flex-col justify-center items-center">
-                    <h1 className="text-3xl text-azull01 cronometro">
-                        {this.state.seconds}
-                    </h1>
-                </div>
+        <div className="flex flex-row justify-center w-48 md:w-96 m-4">
+          <button type='button' onClick={this.startStop()} className="bg-azull01 text-white rounded-lg scroll-py-24 w-36 m-4 px-4 py-3">
+            Iniciar/Parar
+          </button>
 
-                <div className="flex flex-row justify-center w-48 md:w-96 m-4">
-                    <button onClick={this.startStop()} className="bg-azull01 text-white rounded-lg scroll-py-24 w-36 m-4 px-4 py-3">
-                    Iniciar/Parar
-                    </button>
-
-                    <button onClick={this.zero()} className="bg-red-600 text-white rounded-lg scroll-py-24 w-36 m-4 px-4 py-3">
-                        Zerar
-                    </button>
-                </div>
-            </>
-        );
-    }
+          <button type='button' onClick={this.zero()} className="bg-red-600 text-white rounded-lg scroll-py-24 w-36 m-4 px-4 py-3">
+            Zerar
+          </button>
+        </div>
+      </>
+    );
+  }
 }
 
 export default Timer;
--- src/index.jsx
+++ src/index.jsx
@@ -1,34 +1,38 @@
-import React from "react"; /* importa biblioteca React */
-import ReactDOM from "react-dom/client";
-import Timer from "./components/Exemplo01"; //importa componente
-import "./index.css";
+import React from 'react'; /* importa biblioteca React */
+import ReactDOM from 'react-dom/client';
+import Timer from './components/Exemplo01'; // importa componente
+import './index.css';
 
 // class OlaMundo extends React.Component{ /* componente (usado na metodologia de classe)*/
 //     render(){
-//         return <h1>Olá {this.props.nome}</h1> /* repassa propriedades para filho (comunicação direta = cima para baixo)*/
+//         return <h1>Olá {this.props.nome}</h1> /* repassa propriedades (comunicação direta)*/
 //     }
 // }
 
-function OlaMundo(props){ /* componente funcional */
-    return <h1>Olá {props.nome}</h1>
+function OlaMundo(props) { /* componente funcional */
+  const {nome} = props; /* desestruturação */
+  return (
+    <h1>
+      Olá
+      {nome}
+    </h1>
+  );
 }
 
-class Pagina01 extends React.Component{ /* componente metodologia de classe*/
-    render(){
-        return (
-            <> {/* fragmento para imprimir mais de uma vez (return só retorna uma vez) */}
-                <OlaMundo nome={this.props.nome[0]}/>
-                <OlaMundo nome={this.props.nome[1]}/>
-            </>
-        )
-    }
+function Pagina01 () { /* componente metodologia de classe */
+  const { nome } = props;
+  return (
+    <div>
+      <OlaMundo nome={nome[0]} />
+      <OlaMundo nome={nome[1]} />
+    </div>
+  );
 }
 
-const root = ReactDOM.createRoot(document.getElementById("root")); /* renderiza o componente */
-//root.render(<Pagina01 nome={["Mariana", "Mari"]}/>);
-//root.render(<Timer />); //chama componente Timer do exemplo 01
+const root = ReactDOM.createRoot(document.getElementById('root')); /* renderiza o componente */
+// root.render(<Pagina01 nome={["Mariana", "Mari"]}/>);
+// root.render(<Timer />); //chama componente Timer do exemplo 01
 root.render(
-    <>
-        <Timer start={0} ms={1000}></Timer>
-    </>
+  <Timer start={0} ms={1000} />,
+  <Pagina01 />,
 );
